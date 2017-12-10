094de9c4edfebcb150bdd1a14e0bac1c7067e865
vysarge <vysarge@gmail.com>
2017-12-10 12:57:28 -0500

    Update testbench

--- os_arch/tb.py
+++ os_arch/tb.py
@@ -136,8 +136,9 @@ if __name__ == "__main__":
         out_chn = [16, 16, 8, None]
         block_size = [8, 8, 8, None]
         num_nonzeros = [[8, 8, 8, None], [4, 4, 4, None], [2, 2, 2, None], [1, 1, 1, None]]
-        #pruner_name = "NaivePruner"
-        pruner_name = "ClusteredPruner"
+        pruner_name = "NaivePruner"
+        print("Outputs may take a long time to start appearing (~5 mins); be patient!")
+        #pruner_name = "ClusteredPruner"
 
         # Load weights and biases from file
         saved_weights = np.load("../cnn/98.npy")
